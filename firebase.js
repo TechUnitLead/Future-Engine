ade09f77a57b786804f27e1a4b6557dfa0a68b45
Lavinia <lavinia.thosatriavi@gmail.com>
2024-07-19 17:56:42 +0700

    client-side app (front-end)

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDezaNj84fzbKIolQtKBMInWcHZDe_4XYQ",
+  authDomain: "my-rouge-firebase-app.firebaseapp.com",
+  projectId: "my-rouge-firebase-app",
+  storageBucket: "my-rouge-firebase-app.appspot.com",
+  messagingSenderId: "696844850614",
+  appId: "1:696844850614:web:2a199b3dde8f9c42d4dd17"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
